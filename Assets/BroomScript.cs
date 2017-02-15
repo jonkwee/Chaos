3b61d2ad4ad5826eb3931cfbcef2ef49d3f5ee55
jonkwee <jonkwee@gmail.com>
2017-02-15 15:38:46 -0600

    Initial Commit

---
+++ Assets/BroomScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BroomScript : MonoBehaviour {
+
+	private Vector3 startPos;
+	private Vector3 endPos;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position += new Vector3 (0f, 0f, 1f) * 0.5f;
+		}
+		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
+			transform.position -= new Vector3 (0f, 0f, 1f) * 0.5f;
+		}
+	}
+}
---
+++ Assets/GameScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour {
+
+	public GameObject broom;
+	public GameObject obstacle;
+
+	private Vector3 startingPos = new Vector3(-6f,-1f,0f);
+	private Vector3 startingRot = new Vector3 (-56f, -137f, 46f);
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (obstacle, new Vector3 (-4f, -1f, 0f), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
